bdeabb51e326e66a7b87a4bb7b8bf4cb5b442791
TlhCode <136689385+tlhcodee@users.noreply.github.com>
2024-11-03 19:10:12 +0300

    Delete CAC directory

--- CAC/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CAC
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CAC";
-        public const string PLUGIN_NAME = "CAC";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
